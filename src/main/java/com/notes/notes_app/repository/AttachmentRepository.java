f782b535e36d43103f155e0fba0c9b175bd435d2
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-15 16:50:53 +0100

    Repository level files
    Implementing JpaRepository for the existing models

--- src/main/java/com/notes/notes_app/repository/AttachmentRepository.java
+++ src/main/java/com/notes/notes_app/repository/AttachmentRepository.java
@@ -1,11 +1,11 @@
 package com.notes.notes_app.repository;
 
-import com.notes.notes_app.model.Note;
+import com.notes.notes_app.model.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
-    List<Note> findByUserId(Long userId);
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+    List<Attachment> findByNoteId(Long noteId);
 }
