36621a3f36f1d1cb3d937f1c57433b7d3aac21f5
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-16 22:43:01 +0100

    User controller and service
    - Basic Controller and CRUD services for the User model
    - Checks, logging, errors TBA after primary functionalities are implemented

--- src/main/java/com/notes/notes_app/controller/UserController.java
+++ src/main/java/com/notes/notes_app/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.notes.notes_app.controller;
 
+import com.notes.notes_app.model.User;
+import com.notes.notes_app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{username}")
+    public ResponseEntity<Optional<User>> findByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(userService.findByUsername(username));
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
 }
--- src/main/java/com/notes/notes_app/service/UserService.java
+++ src/main/java/com/notes/notes_app/service/UserService.java
@@ -1,4 +1,25 @@
 package com.notes.notes_app.service;
 
+import com.notes.notes_app.model.User;
+import com.notes.notes_app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
 }
