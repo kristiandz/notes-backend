91b92e1c6138aab37c0e263a43e4bc0428aaf1ec
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-16 22:43:52 +0100

    Category controller and service
    - Basic Controller and CRUD services for the Category model
    - Checks, logging, errors TBA after primary functionalities are implemented

--- src/main/java/com/notes/notes_app/controller/CategoryController.java
+++ src/main/java/com/notes/notes_app/controller/CategoryController.java
@@ -1,4 +1,31 @@
 package com.notes.notes_app.controller;
 
+import com.notes.notes_app.model.Category;
+import com.notes.notes_app.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<Optional<Category>> getCategory(@PathVariable Long categoryId) {
+        return ResponseEntity.ok(categoryService.getCategory(categoryId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.createCategory(category));
+    }
 }
--- src/main/java/com/notes/notes_app/service/CategoryService.java
+++ src/main/java/com/notes/notes_app/service/CategoryService.java
@@ -1,4 +1,27 @@
 package com.notes.notes_app.service;
 
+import com.notes.notes_app.model.Category;
+import com.notes.notes_app.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    // name or id ?
+    public Optional<Category> getCategory(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
 }
