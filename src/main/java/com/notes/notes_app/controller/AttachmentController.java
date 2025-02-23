7e13208b80a2a9dbd4aebcca0245ad95a007fef0
kristiandz <36107078+kristiandz@users.noreply.github.com>
2025-02-23 21:44:08 +0100

    Updated attachment service and controller
    - Adding the delete attachment endpoint
    - Modifications for the save and update attachments
    - Checks for existing attachments to overwrite and prevent db issues

--- src/main/java/com/notes/notes_app/controller/AttachmentController.java
+++ src/main/java/com/notes/notes_app/controller/AttachmentController.java
@@ -6,12 +6,10 @@ import com.notes.notes_app.repository.AttachmentRepository;
 import com.notes.notes_app.service.AttachmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/attachments")
@@ -23,10 +21,22 @@ public class AttachmentController {
     @GetMapping("/{id}")
     public ResponseEntity<byte[]> downloadAttachment(@PathVariable Long id) {
         Attachment attachment = attachmentService.getAttachmentById(id);
-        System.out.println("TEST");
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(attachment.getFileType()))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + attachment.getFileName() + "\"")
                 .body(attachment.getData());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAttachment(@PathVariable Long id) {
+        try {
+            attachmentService.deleteAttachmentById(id);
+            return ResponseEntity.ok("Attachment deleted successfully");
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred while deleting the attachment: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/notes/notes_app/service/AttachmentService.java
+++ src/main/java/com/notes/notes_app/service/AttachmentService.java
@@ -3,15 +3,23 @@ package com.notes.notes_app.service;
 import com.notes.notes_app.errorHandle.ResourceNotFoundException;
 import com.notes.notes_app.model.Attachment;
 import com.notes.notes_app.model.AttachmentDTO;
+import com.notes.notes_app.model.Note;
 import com.notes.notes_app.repository.AttachmentRepository;
+import com.notes.notes_app.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class AttachmentService {
     @Autowired
     private AttachmentRepository attachmentRepository;
+    @Autowired
+    private NoteRepository noteRepository;
 
     public List<Attachment> getFilesByNoteId(Long noteId) {
         return attachmentRepository.findByNoteId(noteId);
@@ -26,7 +34,33 @@ public class AttachmentService {
         return attachmentRepository.findAttachmentMetadataByNoteId(noteId);
     }
 
-    public void saveAttachment(Attachment attachment) {
+    public void saveAttachment(MultipartFile file, Long noteId) throws IOException {
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note not found"));
+
+        Attachment attachment = new Attachment();
+        attachment.setFileName(file.getOriginalFilename());
+        attachment.setFileType(file.getContentType());
+        attachment.setData(file.getBytes());
+        attachment.setNote(note);
         attachmentRepository.save(attachment);
     }
+
+    public void reassociateAttachmentToNote(Long attachmentId, Long noteId) {
+        Attachment attachment = attachmentRepository.findById(attachmentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Attachment not found with id: " + attachmentId));
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note not found with id: " + noteId));
+        attachment.setNote(note);
+        attachmentRepository.save(attachment);
+    }
+
+    @Transactional
+    public void deleteAttachmentById(Long attachmentId) {
+        if (!attachmentRepository.existsById(attachmentId)) {
+            throw new ResourceNotFoundException("Attachment not found with id: " + attachmentId);
+        }
+        attachmentRepository.deleteById(attachmentId);
+    }
+
 }
